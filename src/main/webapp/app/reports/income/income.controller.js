566bf9c7736dbdd474f3576a50816dfe0803e79f
David L. Whitehurst <david@ciwise.com>
2017-02-27 19:36:06 -0500

    Issue#4 - Ongoing

--- src/main/webapp/app/reports/income/income.controller.js
+++ src/main/webapp/app/reports/income/income.controller.js
@@ -4,9 +4,12 @@
     angular
         .module('blackholeApp')
         .controller('IncomeController', IncomeController);
-    IncomeController.$inject = ['$scope', '$state'];
+    IncomeController.$inject = ['$scope', '$state', 'LastEntry', 'DebitTrialBalance', 'CreditTrialBalance'];
 
-    function IncomeController ($scope, $state) {
+    function IncomeController ($scope, $state, LastEntry, DebitTrialBalance, CreditTrialBalance) {
         var vm = this;
+        $scope.lastEntry = LastEntry.get();
+        $scope.debitbalances = DebitTrialBalance.query({size:200});
+        $scope.creditbalances = CreditTrialBalance.query({size:200});
     }
 })();
