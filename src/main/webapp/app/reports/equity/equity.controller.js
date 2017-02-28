94dd1d55bdd21acfa81199a89a80add14018d83b
David L. Whitehurst <david@ciwise.com>
2017-02-27 19:35:14 -0500

    Issue#5 - Ongoing

--- src/main/webapp/app/reports/equity/equity.controller.js
+++ src/main/webapp/app/reports/equity/equity.controller.js
@@ -4,9 +4,10 @@
     angular
         .module('blackholeApp')
         .controller('EquityController', EquityController);
-    EquityController.$inject = ['$scope', '$state'];
+    EquityController.$inject = ['$scope', '$state', 'LastEntry'];
 
-    function EquityController ($scope, $state) {
+    function EquityController ($scope, $state, LastEntry) {
         var vm = this;
+        $scope.lastEntry = LastEntry.get();
     }
 })();
